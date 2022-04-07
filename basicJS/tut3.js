2532762b2850d566951b129b82f64a6257b248c0
MASOOD BAIG <baiginsha18@gmail.com>
2022-04-07 15:01:20 +0530

    first commit

---
+++ basicJS/tut3.js
@@ -0,0 +1,36 @@
+console.log('tut3')
+//variable in js
+//var, let, const
+var insha;
+//variable declare kiya h
+
+var insha=55;
+//variable insha ko value assine ki history;
+
+var insha="66";
+//Agr string ke ander liko ge to airthmetic operation perform nahi kr pw ge.
+  console.log("insha ")
+  // Rules for creating Javascript variable
+   //1.Cannot start with number
+  //2.can start with letter, number, or $
+  //3.Are case sensistive
+   var city='Delhi'
+
+const ownersname ='hariram' 
+//ownersname="insha";
+
+//console.log('ownersname');
+ var insha=2
+ insha="9"
+ console.log()
+  var z ="hello world";
+  var x =100.25;
+  z="error"
+  document.write(z);
+  document.write(x);
+
+
+
+
+
+
